a9384b3f518cb9b2af0f7e3dd521fda5a78360bf
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2025-12-04 18:34:00 +0800

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ export default function App() {
   return (
     <main>
       <Navbar />
+
+      <div className="container mx-auto p-4 flex flex-col md:flex-row gap-4">
+        <div className="flex-1 bg-red-100">Sidebar</div>
+        <div className="flex-2 bg-green-100">Content</div>
+      </div>
     </main>
   );
 }
