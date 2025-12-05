c40e26cdc04e3507f77c562cb51ece8fe861b67a
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2025-12-05 09:21:35 +0800

    Persist data

--- src/stores/useNoteStore.ts
+++ src/stores/useNoteStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { v4 as uuidv4 } from "uuid";
 
 import { type Note } from "@/types";
@@ -13,35 +14,41 @@ type NoteStore = {
   deleteNote: (note: Note) => void;
 };
 
-const useNoteStore = create<NoteStore>((set) => ({
-  notes: [],
+const useNoteStore = create<NoteStore>()(
+  persist(
+    (set) => ({
+      notes: [],
 
-  createNote: () => {
-    const note: Note = {
-      id: uuidv4(),
-      title: "Untitled note",
-      createdAt: Date.now(),
-    };
+      createNote: () => {
+        const note: Note = {
+          id: uuidv4(),
+          title: "Untitled note",
+          createdAt: Date.now(),
+        };
 
-    set((state) => ({
-      notes: [note, ...state.notes],
-      selectedNote: note,
-      isEditing: true,
-    }));
-  },
+        set((state) => ({
+          notes: [note, ...state.notes],
+          selectedNote: note,
+          isEditing: true,
+        }));
+      },
 
-  selectNote: (note: Note) => {
-    set({ selectedNote: note });
-  },
+      selectNote: (note: Note) => {
+        set({ selectedNote: note });
+      },
 
-  deleteNote: (note: Note) => {
-    set((state) => ({
-      notes: state.notes.filter((n) => n.id !== note.id),
-      selectedNote:
-        state.selectedNote?.id === note.id ? undefined : state.selectedNote,
-      isEditing: state.selectedNote?.id === note.id ? false : state.isEditing,
-    }));
-  },
-}));
+      deleteNote: (note: Note) => {
+        set((state) => ({
+          notes: state.notes.filter((n) => n.id !== note.id),
+          selectedNote:
+            state.selectedNote?.id === note.id ? undefined : state.selectedNote,
+          isEditing:
+            state.selectedNote?.id === note.id ? false : state.isEditing,
+        }));
+      },
+    }),
+    { name: "notes" }
+  )
+);
 
 export default useNoteStore;
